4d9cbcf5e7c30361f5b9da0e53f30013b04d368b
Darya Domoroschenko <DADomoroschenko@greenatom.ru>
2021-10-08 18:07:58 +0300

    Создала компоненты страниц

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  ROOM_COUNT: 5,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App roomCount={Setting.ROOM_COUNT}/>
   </React.StrictMode>,
   document.getElementById('root'));
