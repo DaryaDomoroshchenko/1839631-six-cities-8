a2a183b2b346f64e9bbde9e3e428bea312a7d725
Darya Domoroschenko <DADomoroschenko@greenatom.ru>
2021-11-22 17:00:34 +0300

    Добавила переменные стили у Header

--- project/src/components/header/header.tsx
+++ project/src/components/header/header.tsx
@@ -1,7 +1,8 @@
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { AppRoute } from '../../const';
 import { getIsLoggedInStatus } from '../../store/reducers/user-reducer/selectors';
+import { getClassNames } from '../../utils';
 import UserBlockAuthorized from '../user-block-authorized/user-block-authorized';
 import UserBlockNotAuthorized from '../user-block-not-authorized/user-block-not-authorized';
 
@@ -12,13 +13,20 @@ type HeaderProps = {
 function Header({ showNav }: HeaderProps): JSX.Element {
   const isLoggedIn = useSelector(getIsLoggedInStatus);
 
+  const location = useLocation();
+  const isMainScreen = location.pathname === AppRoute.Main;
+
   return (
     <header className="header">
       <div className="container">
         <div className="header__wrapper">
           <div className="header__left">
             <Link
-              className="header__logo-link header__logo-link--active"
+              // className="header__logo-link header__logo-link--active"
+              className={getClassNames(
+                'header__logo-link',
+                {'header__logo-link--active': isMainScreen},
+              )}
               to={AppRoute.Main}
             >
               <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
